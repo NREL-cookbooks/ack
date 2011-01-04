a2dfa4ec578408b9f826ba93007f5824a88a5ae0
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-01-04 23:53:39 +0000

    Replace with a newer version of ack so rspec files get included in ruby searches by default.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,17 +5,36 @@
 # Copyright 2010, NREL
 #
 
+package_name = "ack"
+binary_path = "/usr/bin/ack"
+
 case node[:platform]
 when "ubuntu", "debian"
-  package "ack-grep" do
-    action :install
-  end
+  package_name = "ack-grep"
+  binary_path = "/usr/bin/ack-grep"
+end
 
+package package_name do
+  action :install
+end
+
+# In Ubuntu the package gets installed as "ack-grep". To keep things consistent
+# across platforms, we want to symlink "ack" so that also works.
+if(File.basename(binary_path) != "ack")
   link "/usr/bin/ack" do
-    to "/usr/bin/ack-grep"
+    to binary_path
   end
-else
-  package "ack" do
-    action :install
-  end
+end
+
+# Replace the installed version of the ack binary with a newer version. Since
+# ack is really just a standalone binary, this isn't a huge deal.
+#
+# We're doing this to get at least version 1.93_01 where .spec files are
+# automatically parsed as ruby.
+cookbook_file "ack" do
+  source "ack"
+  path binary_path
+  mode "0755"
+  owner "root"
+  group "root"
 end
