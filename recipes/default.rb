5b4b2cacd24af2f19217b116fd495abe1530a61c
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-01-04 23:37:52 +0000

    Install ack by default and symlink as 'ack' on ubuntu too.

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,10 @@ when "ubuntu", "debian"
   package "ack-grep" do
     action :install
   end
+
+  link "/usr/bin/ack" do
+    to "/usr/bin/ack-grep"
+  end
 else
   package "ack" do
     action :install
