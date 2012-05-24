80b4da72b3008dd42cd4f317db4b0cdb977325ea
Muerdter, Nick <nick.muerdter@nrel.gov>
2012-05-24 04:52:46 +0000

    Install newer ack from rpmforge.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2010, NREL
 #
 
-include_recipe "yum::epel"
+include_recipe "yum::rpmforge"
 
 package_name = "ack"
 binary_path = "/usr/bin/ack"
@@ -26,17 +26,4 @@ if(File.basename(binary_path) != "ack")
   link "/usr/bin/ack" do
     to binary_path
   end
-end
-
-# Replace the installed version of the ack binary with a newer version. Since
-# ack is really just a standalone binary, this isn't a huge deal.
-#
-# We're doing this to get at least version 1.93_01 where .spec files are
-# automatically parsed as ruby.
-cookbook_file "ack" do
-  source "ack"
-  path binary_path
-  mode "0755"
-  owner "root"
-  group "root"
 end
