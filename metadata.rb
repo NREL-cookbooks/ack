72e859358c4c9c98cb3f563d5a30684259be7949
Nick Muerdter <nick.muerdter@nrel.gov>
2014-03-05 13:39:42 -0700

    Bump to v0.2.0

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ maintainer       "Nick Muerdter"
 maintainer_email "nick.muerdter@nrel.gov"
 description      "Installs/Configures ack"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.1"
+version          "0.2.0"
 
 depends "perl"
