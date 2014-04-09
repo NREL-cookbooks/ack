df1048b46095d2aed16f3317c4723935cff3b118
Nick Muerdter <nick.muerdter@nrel.gov>
2014-04-09 16:25:30 -0600

    Adding name to cookbook metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
+name             'ack'
 maintainer       "Nick Muerdter"
 maintainer_email "nick.muerdter@nrel.gov"
 description      "Installs/Configures ack"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.0"
+version          "0.2.1"
 
 depends "perl"
